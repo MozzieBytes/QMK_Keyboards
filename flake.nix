08a5371d5971cc64210146d4ca81f63d1cfeded6
Mozzie <15324906+MozzieBytes@users.noreply.github.com>
2025-12-17 23:48:05 +0000

    Feat: Nix flake integration
    Feat: Setting up qmk devShell.
    
    - Set up a devShell that builds and configures the qmk dev environment.
      Issue is this creates a long build time when first checking-out the
      dev environment.
    
    Feat: Keyboard firmware can now be built with nix

--- flake.nix
+++ flake.nix
@@ -7,14 +7,51 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    nix-global,
-    ...
-    }: nix-global.lib.mkDevEnv {} (
+  outputs =
+    {
+      nix-global,
+      ...
+    }:
+    nix-global.lib.mkDevEnv { } (
       pkgs: corePkgs:
+      let
+        qmk-src = pkgs.fetchFromGitHub {
+          owner = "qmk";
+          repo = "qmk_firmware";
+          tag = "0.31.1";
+          sha256 = "sha256-jibfaqh4uTx2sWyQ9V5qiUoe9B+8jK3g1WXNfs2xArg=";
+          fetchSubmodules = true;
+        };
+        qmk = pkgs.callPackage ./utils/qmk.nix { inherit qmk-src; };
+      in
       {
+        packages.compile = with qmk; {
+          gmmk_pro = compile {
+            name = "gmmk_pro";
+            config = ./keyboards/GMMK_Pro/gmmk_pro_rev1_ansi.json;
+          };
+          corne = compile {
+            name = "corne";
+            config = ./keyboards/Corne/crkbd_rev1.json;
+            is_rp2040 = true;
+          };
+        };
         devShells.default = pkgs.mkShell {
-          buildInputs = corePkgs;
+          buildInputs =
+            with pkgs;
+            [
+              qmk
+              dos2unix
+            ]
+            ++ corePkgs;
+          shellHook = ''
+            export QMK_HOME=$PWD/qmk-home
+            mkdir -p $QMK_HOME
+            if [ ! -e "$QMK_HOME/qmk_firmware" ]; then
+              ln -s ${qmk-src} "$QMK_HOME/qmk_firmware"
+            fi
+            [ -d "$QMK_HOME/qmk_firmware/.git/modules" ] || qmk setup -y
+          '';
         };
       }
     );
